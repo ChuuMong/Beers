7247dd567426f92aba47d2b921e77aa6d0e2a636
JongHun Lee <whdcn@naver.com>
2018-12-10 17:05:07 +0900

    impl #6 - 맥주 필터 기능 추가

---
+++ app/src/main/java/io/chuuhomg/beers/ui/weiget/OnSeekBarProgressChangedListener.kt
@@ -0,0 +1,18 @@
+package io.chuuhomg.beers.ui.weiget
+
+import android.widget.SeekBar
+
+
+/**
+ * Created by Home on 2018-12-10.
+ */
+abstract class OnSeekBarProgressChangedListener : SeekBar.OnSeekBarChangeListener {
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+    }
+}
